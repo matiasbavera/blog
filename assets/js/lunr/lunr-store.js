17193ab3ab21fbf0d9641f9efb9db93562d39987
Matias Bavera <matiasbavera@gmail.com>
2020-11-08 23:22:01 -0300

    Added Database questions

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Test Your Knowledge Of Databases",
+        "excerpt":"General What do you understand by ‘Database’? Define DBMS. Define RDBMS. How is it different from DBMS? Advantages of DBMS. What is Data Redundancy? What are B-trees? What is database partitioning? What is an Object-Oriented Database Model? What is a hashing technique in a database? What does data independence mean?...","categories": [],
+        "tags": [],
+        "url": "https://matiasbavera.github.io/blog/test-your-knowledge-of-databases/",
+        "teaser": "https://matiasbavera.github.io/blog/assets/images/profile.png"
+      }]
